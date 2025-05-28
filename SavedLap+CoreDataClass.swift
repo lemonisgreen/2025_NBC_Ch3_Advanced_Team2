c5c3cf2fd5f3e55b4e32039cd683d9b1a8e2c1c1
LEMONY <jin.lee1028@gmail.com>
2025-05-28 10:09:31 +0900

    [Feat] #1 - Stopwatch 최첨단 수동 머지

---
+++ SavedLap+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  SavedLap+CoreDataClass.swift
+//  Allarm
+//
+//  Created by Jin Lee on 5/28/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(SavedLap)
+public class SavedLap: NSManagedObject {
+
+}
